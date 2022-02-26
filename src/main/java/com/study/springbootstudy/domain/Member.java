089aa672dbb5ec229a3bc9d038e775f394e7a559
boxak <boxak@naver.com>
2022-02-27 00:22:10 +0900

    2022-02-26 AllArgsConstructor, NoArgsConstructor 테스트

--- src/main/java/com/study/springbootstudy/domain/Member.java
+++ src/main/java/com/study/springbootstudy/domain/Member.java
@@ -1,7 +1,11 @@
 package com.study.springbootstudy.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @ToString(exclude = "userName")
 public class Member {
     private String userId;
--- src/test/java/com/study/springbootstudy/LombokTests.java
+++ src/test/java/com/study/springbootstudy/LombokTests.java
@@ -2,8 +2,11 @@ package com.study.springbootstudy;
 
 import com.study.springbootstudy.domain.Board;
 import com.study.springbootstudy.domain.Member;
+import com.study.springbootstudy.domain.Student;
+import com.study.springbootstudy.domain.UserItem;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -48,6 +51,18 @@ public class LombokTests {
     }
 
     @Test
+    public void allArgsConstructor() {
+        Board board = new Board(1, "title", "content1", "writer1", LocalDateTime.now());
+        System.out.println(board);
+    }
+
+    @Test
+    public void allArgsConstructor2() {
+        Member member = new Member("userId1", "password1", "userName1");
+        System.out.println(member);
+    }
+
+    @Test
     public void testBoard() {
         Board board1 = new Board();
         board1.setBoardNo(1);
@@ -70,6 +85,61 @@ public class LombokTests {
         System.out.println("저장된 데이터 수 : " + boardSet.size());
 
         Iterator<Board> iterator = boardSet.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    @Test
+    public void testUserItem() {
+        UserItem userItem1 = new UserItem();
+        userItem1.setUserNo(1);
+        userItem1.setItemId(100);
+        userItem1.setDescription("userItem1");
+
+        UserItem userItem2 = new UserItem();
+        userItem2.setUserNo(1);
+        userItem2.setItemId(200);
+        userItem2.setDescription("userItem2");
+
+        UserItem userItem3 = new UserItem();
+        userItem3.setUserNo(1);
+        userItem3.setItemId(200);
+        userItem3.setDescription("userItem3");
+
+        Set<UserItem> userItemSet = new HashSet<>();
+
+        userItemSet.add(userItem1);
+        userItemSet.add(userItem2);
+        userItemSet.add(userItem3);
+
+        System.out.println("저장된 데이터 수 : " + userItemSet.size());
+
+        Iterator<UserItem> iterator = userItemSet.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    @Test
+    public void testStudent() {
+        Student student1 = new Student();
+        student1.setStudentNo(1);
+        student1.setName("Alex");
+
+        Student student2 = new Student();
+        student2.setStudentNo(2);
+        student2.setName("Alex");
+
+        Set<Student> studentSet = new HashSet<>();
+
+        studentSet.add(student1);
+        studentSet.add(student2);
+
+        System.out.println("저장된 데이터 수 : " + studentSet.size());
+
+        Iterator<Student> iterator = studentSet.iterator();
+
         while(iterator.hasNext()) {
             System.out.println(iterator.next());
         }
