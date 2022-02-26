bff26b9eedb87e0a2127d39142dd5d9ba43f406c
boxak <boxak@naver.com>
2022-02-26 23:43:13 +0900

    2022-02-26 equalsAndHashCode 의 of 파라미터는 해당 필드 기준으로 equals를 판단한다.

--- src/test/java/com/study/springbootstudy/LombokTests.java
+++ src/test/java/com/study/springbootstudy/LombokTests.java
@@ -4,6 +4,10 @@ import com.study.springbootstudy.domain.Board;
 import com.study.springbootstudy.domain.Member;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class LombokTests {
 
     @Test
@@ -41,6 +45,34 @@ public class LombokTests {
     public void toStringExclude() {
         Member member = new Member();
         System.out.println(member);
+    }
+
+    @Test
+    public void testBoard() {
+        Board board1 = new Board();
+        board1.setBoardNo(1);
+        board1.setTitle("title1");
+
+        Board board2 = new Board();
+        board2.setBoardNo(2);
+        board2.setTitle("title2");
+
+        Board board3 = new Board();
+        board3.setBoardNo(2);
+        board3.setTitle("title3");
+
+        Set<Board> boardSet = new HashSet<>();
+
+        boardSet.add(board1);
+        boardSet.add(board2);
+        boardSet.add(board3);
+
+        System.out.println("저장된 데이터 수 : " + boardSet.size());
+
+        Iterator<Board> iterator = boardSet.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 
 }
